356f08d67d3916c13c6c470121a419673f3945c1
Do Hoang Viet <>
2023-07-05 15:29:29 +0700

    Coffee animation

--- tutorials/src/router/index.js
+++ tutorials/src/router/index.js
@@ -3,19 +3,20 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/coffee_animation",
+    name: "Coffee Animation",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/CoffeeAnimation.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
