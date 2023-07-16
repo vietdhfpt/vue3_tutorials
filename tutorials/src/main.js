eb0e9daea280844f6a985eb08e55c774668b1d3d
Do Hoang Viet <>
2023-07-16 19:45:15 +0700

    Learn scss (P1)

--- tutorials/src/main.js
+++ tutorials/src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import style from "./styles/style.css";
+import resetStyle from "./styles/reset.css";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router, style, resetStyle).mount("#app");
--- tutorials/src/views/HomeView.vue
+++ tutorials/src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <header class="header">
+      <div class="container header__inner">
+        <a href="/" class="logo">
+          <span class="logo__text">Play</span>ground
+        </a>
+        <nav class="header__menu-list">
+          <ul class="header__menu-item">
+            <a href="#" class="header__menu-link">Events</a>
+          </ul>
+          <ul class="header__menu-item">
+            <a href="#" class="header__menu-link">Pricing</a>
+          </ul>
+          <ul class="header__menu-item">
+            <a href="#" class="header__menu-link">Blog</a>
+          </ul>
+          <ul class="header__menu-item">
+            <a href="#" class="header__menu-link btn btn--border btn--rounded"
+              >Login</a
+            >
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <section class="banner">
+      <div class="container">
+        <h1 class="banner__heading">The Kids playground</h1>
+        <p class="banner__desc">
+          The Playground, designed by nonprofit organization to<br />accessible
+          for all children
+        </p>
+        <div class="banner__buttons">
+          <a href="#" class="btn btn--border btn--rounded">Explore</a>
+          <a href="#" class="btn btn--primary btn--rounded has--shadow"
+            >Buy Tickets</a
+          >
+        </div>
+      </div>
+    </section>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+};
 </script>
+<style></style>
